6efa6e9aeaebda674630939e392c7b0f6e702590
ChanduKSDIS <chandu@getdelos.com>
2023-11-27 19:50:38 +0530

    Basic Class component Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassComponent from './Components/ClassComponents';
+
+function App() {
+  return (
+    <div>
+
+      <ClassComponent />
+
+    </div>
+  );
+}
+
+export default App;
