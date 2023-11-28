2228b175fd715c60d80f18eaa1ff988eb99f3781
ChanduKSDIS <chandu@getdelos.com>
2023-11-28 12:50:34 +0530

    Class component

--- src/Components/ClassComponents.js
+++ src/Components/ClassComponents.js
@@ -6,7 +6,7 @@ class ClassComponent extends React.Component {
         return <div>
 
             <h1>This is class Component</h1>
-            <h2>This is second line of class componet</h2>
+            <h2>This is second line of class componet.</h2>
         </div>
 
     }
